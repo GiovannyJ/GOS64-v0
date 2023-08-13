c289680b66905febc32447206c715829a168b6dc
GiovannyJ <94592434+GiovannyJ@users.noreply.github.com>
2023-08-12 21:25:09 -0400

    init commit

---
+++ src/impl/kernel/main.c
@@ -0,0 +1,9 @@
+#include "print.h"
+
+void kernel_main(){
+    print_clear();
+    print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+    print_str("Welcome to GOS64");
+    print_newline();
+    print_str("This is an OS that can do a bunch of cool shit");
+}
